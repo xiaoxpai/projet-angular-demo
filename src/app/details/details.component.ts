9f7d0b814c658e890b84d4905344aab6ea70af4b
F1686352 <hzlh-mis-xiao.lh@foxconn.com>
2024-07-24 14:18:31 +0800

    feat: 新增响应式表单

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,7 +1,7 @@
 import { Component,inject,OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {CommonModule} from '@angular/common';
-
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';//12添加表单
 
 import { HousingService } from '../housing-location/housing.service';
 import { Housinglocation } from '../housing-location/housinglocation';
@@ -9,7 +9,7 @@ import { Housinglocation } from '../housing-location/housinglocation';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -25,6 +25,21 @@ export class DetailsComponent   {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
+  }
+
 }
 //import {Component, inject} from '@angular/core';
  
--- src/app/housing-location/housing.service.ts
+++ src/app/housing-location/housing.service.ts
@@ -124,4 +124,10 @@ export class HousingService {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
 
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
+  }
+
 }
